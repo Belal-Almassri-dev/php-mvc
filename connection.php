25b3371bb2ad4b0852b971a1d5e769f19d1d67a7
Belal Almassri <belal.almassri@gmail.com>
2016-06-02 01:36:55 +0100

    xx

--- connection.php
+++ connection.php
@@ -6,7 +6,7 @@
     public static function getConnect() {
       if (!isset(self::$instance)) {
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-        self::$instance = new PDO('mysql:host=localhost;dbname=plymouth_main', 'plymouth_main', '47zN(}.eX?9s', $pdo_options);
+        self::$instance = new PDO('mysql:host=localhost;dbname=?????', '???', '????', $pdo_options);
       }
       return self::$instance;
     }
